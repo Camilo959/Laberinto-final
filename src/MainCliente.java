31c413ceb21738304da3380617e3553777c4ee76
DylanFarkas <dylan.farkas@correounivalle.edu.co>
2023-01-17 17:27:08 -0500

    Actualizando

--- src/MainCliente.java
+++ src/MainCliente.java
@@ -5,9 +5,12 @@ import view.VentanaPrincipal;
  * Muestra una interface grafica de usuario
  */
 public class MainCliente {
+    public static int ju = 0;
     public static void main(String[] args) {
         // Vista
         VentanaPrincipal ventana = new VentanaPrincipal();
         ventana.setVisible(true);
+        ju+=1;
+        System.out.println(ju);
     }
 }
